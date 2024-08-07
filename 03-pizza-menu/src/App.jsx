2346ec190e8dfcd4f57ab7fffd7d1fb915d89819
shivPosted <shivpratap621@gmail.com>
2024-08-07 23:55:59 +0530

    completed the Pizza-Menu

--- 03-pizza-menu/src/App.jsx
+++ 03-pizza-menu/src/App.jsx
@@ -120,14 +120,14 @@ function Order({ hour }) {
 }
 
 function Pizza({ pizza: { soldOut, photoName, name, ingredients, price } }) {
-  if (soldOut) return null;
+  // if (soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? 'sold-out' : ''}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name} </h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+        <span>{soldOut ? 'SOLD OUT' : price}</span>
       </div>
     </li>
   );
