e2c36a616230a888097ce7f3ef45d8503e62ea1e
shivPosted <shivpratap621@gmail.com>
2024-08-09 18:12:58 +0530

    build: done with static layout of Far Away App 😄 😄

---
+++ 03-Far-Away-App/src/App.jsx
@@ -0,0 +1,98 @@
+import './style.css';
+const numArray = [];
+for (let i = 0; i <= 19; i++) {
+  numArray.push(i + 1);
+}
+const listItems = [
+  { description: 'Item1', packed: true, quantity: 3, id: 1 },
+  { description: 'Item2', packed: false, quantity: 5, id: 2 },
+];
+console.log(numArray);
+function App() {
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
+}
+
+function Logo() {
+  return (
+    <header>
+      <h1>🏝️ Far Away 💼</h1>
+    </header>
+  );
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('form submitted');
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h2>What do you need for your trip?</h2>
+      <div className="selection-div">
+        <select>
+          {numArray.map(num => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="Item..." className="input-item" />
+        <button className="add-btn">ADD</button>
+      </div>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <section className="packing-list">
+      <ListedItems />
+      <div className="manage-list">
+        <select name="sort-list" id="">
+          <option value="packed">Sort by packed</option>
+          <option value="packed">Sort by packed</option>
+          <option value="packed">Sort by packed</option>
+          <option value="packed">Sort by packed</option>
+        </select>
+        <button className="clear-list-btn">Clear</button>
+      </div>
+    </section>
+  );
+}
+function ListedItems() {
+  return (
+    <ul className="listed-items">
+      {listItems.map(item => (
+        <Item itemObj={item} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+function Item({ itemObj: { description, packed, quantity, id } }) {
+  return (
+    <li className={`listed-item `}>
+      <input id={id} type="checkbox" />
+      <label style={packed ? { textDecoration: 'line-through' } : {}}>
+        {quantity} {description}
+      </label>
+      <button className="close-btn">&times;</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      💼 You have <span className="num-items">0</span> items in your list, and
+      you already packed <span className="num-packed-items">0</span> items (
+      <span className="percentage-packed">0%</span>)
+    </footer>
+  );
+}
+export default App;
