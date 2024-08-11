b04f7c4793bed6b630849cf50b4313aed35b6c2c
shivPosted <shivpratap621@gmail.com>
2024-08-11 19:14:21 +0530

    structuing the files

--- 03-Far-Away-App/src/Components/App.jsx
+++ 03-Far-Away-App/src/Components/App.jsx
@@ -3,7 +3,7 @@ import Logo from './Logo';
 import Form from './Form';
 import PackingList from './PackingList';
 import Stats from './Stats';
-import './style.css';
+import '../style.css';
 
 function App() {
   const [items, setItems] = useState([]);
